e38ef107c16186fa57bec0173f5ec76ee0374c75
lyicecream1012 <liu_ying1012@163.com>
2019-05-29 17:55:08 +0800

    add new file

---
+++ a.py
@@ -0,0 +1,31 @@
+#/usr/bin/python
+#-*- coding: utf-8 -*-
+
+#picklist
+class pickList(object):
+    def __init__(self):
+        pass
+
+    def picklist(self, list):
+        sit_index = 0
+        rm_cnt = 0
+        for i in xrange(sit_index, len(list)):
+            if list[i] == 0:
+                rm_cnt += 1
+            else:
+                if sit_index < i:
+                    list[sit_index] = list[i]
+                sit_index +=1
+        print "rm_cnt:", rm_cnt
+        while(rm_cnt != 0):
+            list.pop()
+            rm_cnt -=1
+
+        return list
+
+if __name__ == '__main__':
+    list = [0,0,1,2,3,0,4,0,0,5,0,0,0,0,6,7,8,9,0,0]
+    #print "list.__len__:", len(list)
+
+    picklist = pickList()
+    print picklist.picklist(list)
---
+++ b.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# yanghui
+
+def print_yanghui(n):
+    if n <= 0:
+        return null
+    listyang = []
+    for i in range(1,n+1):
+        listyang.append(['1'] * i)
+
+    if n > 2:
+        for i in range(2, n):
+            for j in range(1, i):
+                listyang[i][j] = str(int(listyang[i-1][j-1]) + int(listyang[i-1][j]))
+
+    for i in range(0, n):
+        listyang[i] = '\t' * (n-i) + '\t\t'.join(listyang[i])
+        print listyang[i]
+
+
+if __name__ == "__main__":
+    n = 5
+    print 'n=',n
+    print_yanghui(n)
